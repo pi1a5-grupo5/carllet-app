66954d0fa169aca535d158340788865b6bddb800
Pedro Lima <pedrohblima03@gmail.com>
2023-10-05 23:59:40 -0300

    fix: ESlint errors fix

--- src/components/atoms/FloatOptionsButton/index.js
+++ src/components/atoms/FloatOptionsButton/index.js
@@ -1,7 +1,7 @@
-import { Box, Button, IconButton, Text, VStack } from 'native-base'
-import { MaterialIcons } from '@expo/vector-icons'
-import { Animated, Dimensions } from 'react-native'
-import React from 'react'
+import {Box, IconButton, VStack} from 'native-base';
+import {MaterialIcons} from '@expo/vector-icons';
+import {Animated} from 'react-native';
+import React from 'react';
 
 const FlotOptionsButton = ({
   navigation,
@@ -10,39 +10,36 @@ const FlotOptionsButton = ({
   subButtons = [
     {
       icon: 'add',
-      onPress: () => console.log('plus')
+      onPress: () => console.log('plus'),
     },
     {
       icon: 'remove',
-      onPress: () => console.log('minus')
+      onPress: () => console.log('minus'),
     },
     {
       icon: 'edit',
-      onPress: () => console.log('edit')
+      onPress: () => console.log('edit'),
     },
   ],
   ...rest
 }) => {
-  const [isOpen, setIsOpen] = React.useState(false)
-  const [animation] = React.useState(new Animated.Value(0))
-  const screenHeight = Dimensions.get("window").height;
-
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [animation] = React.useState(new Animated.Value(0));
 
   const handlePress = () => {
-    
     Animated.timing(animation, {
       toValue: !isOpen ? 1 : 0,
       duration: 300,
       useNativeDriver: true,
     }).start();
 
-    setIsOpen(!isOpen)
-  }
+    setIsOpen(!isOpen);
+  };
 
   const handleSubButtonPress = (onPress) => {
-    setIsOpen(false)
-    onPress()
-  }
+    setIsOpen(false);
+    onPress();
+  };
 
   return (
     <Box
@@ -51,51 +48,51 @@ const FlotOptionsButton = ({
       right={10}
       {...rest}
     >
-        <Animated.View
-          style={{
-            justifyContent: 'center',
-            alignItems: 'center',
-            position: 'absolute',
-            bottom: 0,
-            zIndex: isOpen ? 1 : -1,
-            opacity: animation.interpolate({
-              inputRange: [0.01, 0.5],
-              outputRange: [0, 1],
-              extrapolate: "clamp",
-            }),
-            transform: [
-              {
-                translateY: animation.interpolate({
-                  inputRange: [0.01, 1],
-                  outputRange: [0, -1 * 50],
-                  extrapolate: "clamp",
-                }),
-              },
-            ],
-          }}
-        >
-          <VStack space={4} alignItems={"center"}>
-            {subButtons.map((subButton, index) => (
-              <IconButton
-                key={index}
-                onPress={() => handleSubButtonPress(subButton.onPress)}
-                variant="solid"
-                shadow={2}
-                rounded="full"
-                width={35}
-                height={35}
-                alignItems={'center'}
-                justifyContent={'center'}
-                p={0}
-                mb={2}
-                _icon={{
-                  as: MaterialIcons,
-                  name: subButton.icon,
-                }}
-              />
-            ))}
-          </VStack>
-        </Animated.View>
+      <Animated.View
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          position: 'absolute',
+          bottom: 0,
+          zIndex: isOpen ? 1 : -1,
+          opacity: animation.interpolate({
+            inputRange: [0.01, 0.5],
+            outputRange: [0, 1],
+            extrapolate: 'clamp',
+          }),
+          transform: [
+            {
+              translateY: animation.interpolate({
+                inputRange: [0.01, 1],
+                outputRange: [0, -1 * 50],
+                extrapolate: 'clamp',
+              }),
+            },
+          ],
+        }}
+      >
+        <VStack space={4} alignItems={'center'}>
+          {subButtons.map((subButton, index) => (
+            <IconButton
+              key={index}
+              onPress={() => handleSubButtonPress(subButton.onPress)}
+              variant="solid"
+              shadow={2}
+              rounded="full"
+              width={35}
+              height={35}
+              alignItems={'center'}
+              justifyContent={'center'}
+              p={0}
+              mb={2}
+              _icon={{
+                as: MaterialIcons,
+                name: subButton.icon,
+              }}
+            />
+          ))}
+        </VStack>
+      </Animated.View>
       <IconButton
         onPress={handlePress}
         bg="white"
@@ -112,7 +109,7 @@ const FlotOptionsButton = ({
         }}
       />
     </Box>
-  )
-}
+  );
+};
 
-export default FlotOptionsButton
+export default FlotOptionsButton;
