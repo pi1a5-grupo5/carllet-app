8eefe74b59900b9ddc87309c508bb2b690cfa336
Pedro Lima <pedrohblima03@gmail.com>
2023-10-26 22:12:29 -0300

    feat: Adequando identidade visual da tela de metas

--- src/screens/Budget/index.js
+++ src/screens/Budget/index.js
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import {View, useWindowDimensions} from 'react-native';
-import {TabView, SceneMap} from 'react-native-tab-view';
-import {FloatOptionsButton, PageContainer} from '../../components';
-import {Box, Text} from 'native-base';
-import {DAYS_OF_WEEK} from '../../constants/date.constants';
-import {StackedBarChart} from 'react-native-chart-kit';
+import { TouchableOpacity, View, useWindowDimensions } from 'react-native';
+import { TabView, SceneMap } from 'react-native-tab-view';
+import { FloatOptionsButton, PageContainer } from '../../components';
+import { Box, Text } from 'native-base';
+import { DAYS_OF_WEEK } from '../../constants/date.constants';
+import { StackedBarChart } from 'react-native-chart-kit';
 
 const GanhosRoute = () => (
   <View>
@@ -22,6 +22,46 @@ const DespesasRoute = () => (
   </View>
 );
 
+const _renderTabBar = props => {
+  const inputRange = props.navigationState.routes.map((x, i) => i);
+
+  console.log(props.navigationState.routes);
+
+  return (
+    <Box>
+      <Box
+        display="flex"
+        flexDirection="row"
+        justifyContent="space-around"
+        backgroundColor="#ff000000"
+        borderRadius={10}
+        marginBottom={10}
+      >
+        {props.navigationState.routes.map((route, i) => {
+          const color = props.position.interpolate({
+            inputRange,
+            outputRange: inputRange.map(
+              inputIndex => (inputIndex === i ? '#000' : '#00000030')
+            ),
+          });
+
+          return (
+            <Box
+              key={i}
+              borderBottomWidth={3}
+              borderBottomColor={color}
+              onPress={() => props.jumpTo(route.key)}
+            >
+              <Text>{route.title}</Text>
+            </Box>
+          );
+        }
+        )}
+      </Box>
+    </Box>
+  );
+};
+
 // eslint-disable-next-line
 const renderScene = SceneMap({
   first: GanhosRoute,
@@ -34,10 +74,14 @@ const Budget = () => {
 
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
-    {key: 'first',
-      title: 'Ganhos'},
-    {key: 'second',
-      title: 'Despesas'},
+    {
+      key: 'first',
+      title: 'Ganhos'
+    },
+    {
+      key: 'second',
+      title: 'Despesas'
+    },
   ]);
 
   const data = {
@@ -64,7 +108,7 @@ const Budget = () => {
         style={{
           alignItems: 'center',
           justifyContent: 'center',
-          marginBottom: 20,
+          marginBottom: 3,
         }}
       >
         <StackedBarChart
@@ -75,8 +119,8 @@ const Budget = () => {
           chartConfig={{
             barPercentage: 0.5,
             backgroundColor: '#fff',
-            backgroundGradientFrom: '#fff',
-            backgroundGradientTo: '#fff',
+            backgroundGradientFrom: '#fff0000',
+            backgroundGradientTo: '#fff0000',
             color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
             labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
             style: {
@@ -89,13 +133,55 @@ const Budget = () => {
         />
       </View>
 
+      <View paddingTop={5} marginBottom={20}>
+        {/* Legenda */}
+        <Box
+          display="flex"
+          flexDirection="row"
+          gap={5}
+        >
+          <Box
+            display="flex"
+            flexDirection="row"
+            alignItems="center"
+            gap={2}
+          >
+            {/* Color */}
+            <Box
+              width={5}
+              height={5}
+              backgroundColor="#00ff0030"
+            />
+            {/* Description */}
+            <Text>Ganhos</Text>
+          </Box>
+          <Box
+            display="flex"
+            flexDirection="row"
+            alignItems="center"
+            gap={2}
+          >
+            {/* Color */}
+            <Box
+              width={5}
+              height={5}
+              backgroundColor="#ff000030"
+            />
+            {/* Description */}
+            <Text>Despesas</Text>
+          </Box>
+        </Box>
+      </View>
 
       <TabView
-        navigationState={{index,
-          routes}}
+        navigationState={{
+          index,
+          routes
+        }}
         renderScene={renderScene}
+        renderTabBar={_renderTabBar}
         onIndexChange={setIndex}
-        initialLayout={{width: layout.width}}
+        initialLayout={{ width: layout.width }}
       />
 
       {/* Botao fixo */}
